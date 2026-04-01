776c3d3eea4ccabd88851bbe5cf9c85e130e0ad7
XA8T <amin.zeka@icloud.com>
2026-04-01 20:34:35 +0100

    Addef blackjack game and refactor menu options inclusive minor bug fix

--- Menu.py
+++ Menu.py
@@ -1,4 +1,8 @@
-from random import randint
+from random import (randint)
+import random
+
+def verlassen():
+    exit()
 
 def taschenrechner():
     while True:
@@ -13,9 +17,7 @@ def taschenrechner():
         elif op == "*":
             print(zahl_1 * zahl_2)
         elif op == "/":
-            if zahl_1 == 0:
-                print("Division durch 0 nicht möglich")
-            elif zahl_2 == 0:
+            if zahl_2 == 0:
                 print("Division durch 0 nicht möglich")
             else:
                 print(zahl_1 / zahl_2)
@@ -46,20 +48,64 @@ def ratespiel():
             else:
                 print("Versuche es erneut")
 
-
         elif zahl > raten:
             print("Die Zahl ist größer als", raten, "versuche es erneut ")
 
         else:
             print("Die Zahl ist kleiner als", raten, "versuche es erneut ")
 
-tools = {
-    "1": taschenrechner,
-    "2": ratespiel,
-}
+def blackjack():
+    player_value = 0
+    dealer_value = 0
+    dealer_value_hidden = 0
+    cards = [i for i in range(1, 11)] * 4
+    random.shuffle(cards)
+    dealer_value += cards.pop()
+    dealer_value_hidden += cards.pop()
+    print("Dealer hat", dealer_value, "+ Unbekannte ")
+    player_value += cards.pop()
+    player_value += cards.pop()
+    print("Du hast", player_value)
+    while True:
+        aktion = str(input("( Hit / Stand ) "))
+        if aktion == "Hit":
+            player_value += cards.pop()
+            print("Du hast", player_value)
+            if player_value > 21:
+                print("Bust! ")
+                return
+            else:
+                pass
+
+        elif aktion == "Stand":
+            break
+    while True:
+        if dealer_value + dealer_value_hidden <= 16:
+            dealer_value += cards.pop()
+            if dealer_value + dealer_value_hidden > 21:
+                print("Dealer hat Verloren! Da er 21 überschritten hat")
+                print("Dealer hat", dealer_value + dealer_value_hidden)
+                return
+            else:
+                pass
+        else:
+            break
+
+    if player_value > dealer_value + dealer_value_hidden:
+        print("Dealer hat nur",dealer_value + dealer_value_hidden, "Du hingegen hast", player_value)
+        print("Du hast Gewonnen! ")
+
+    elif player_value < dealer_value + dealer_value_hidden:
+        print("Du hast nur", player_value, "Der Dealer hingegen hat", dealer_value + dealer_value_hidden)
+        print("Du hast Verloren! ")
+
+    elif player_value == dealer_value + dealer_value_hidden:
+        print("Ihr beide habt", player_value)
+        print("Gleichstand! ")
+        print(dealer_value + dealer_value_hidden)
 
 while True:
-    menu = str(input("Welches Programm soll gestartet werden ( Verlassen / Taschenrechner / Zahlenspiel ) "))
+    menu = str(input("Welches Programm soll gestartet werden ( Verlassen / Taschenrechner / Zahlenspiel / Blackjack ) "))
     if menu == "Taschenrechner":
         taschenrechner()
         pass
@@ -68,5 +114,8 @@ while True:
         ratespiel()
         pass
 
+    elif menu == "Blackjack":
+        blackjack()
+
     elif menu == "Verlassen":
-        exit()
+        verlassen()
